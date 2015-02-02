87a22cd2c8709a17abafa5efc8a6443231bd5d6e
zixie1991 <zixie1991@163.com>
2015-02-03 00:31:22 +0800

    update socket.h

--- src/socket.h
+++ src/socket.h
@@ -1,4 +1,50 @@
 #ifndef SOCKET_H_
 #define SOCKET_H_
 
+
+/** */
+class Socket {
+    public:
+        explicit Socket(int sockfd):
+            sockfd_(sockfd)
+        {}
+
+        ~Socket();
+
+        int fd() const { return sockfd_; }
+
+        /**
+         * @brief abort if address in use
+         */
+        void bindAddress(const InetAddress& localaddr);
+
+        /**
+         * @brief abort if address in use
+         */
+        void listen();
+
+        /**
+         * @brief
+         * @retval On success, return a non-negative integer that is a 
+         *  descriptor for the accepted socket
+         *  On error, -1 is returned
+         */
+        int accept(InetAddress& peeraddr);
+
+        // disable Nagle Aglorithm
+        void disableNagle();
+
+        // disable Linger(取消延迟关闭)
+        void disableLinger();
+
+        // Enable/disable reuseAddr(短连接)
+        void setReuseAddr(bool on);
+
+        /**
+         * @brief Enable/disable SO_KEEPALIVE
+         */
+        void setKeepAlive(bool on);
+
+};
+
 #endif // SOCKET_H_
