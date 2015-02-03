5892ab8407afb923bc9e7062a331159a6c035993
jinleileiking <jinleileiking@gmail.com>
2015-02-03 12:03:52 +0800

    update Buffer interface

--- src/buffer.h
+++ src/buffer.h
@@ -15,7 +15,7 @@
  * +-------------------+------------------+------------------+
  * |                   |                  |                  |
  *
- * 0      <=      readerIndex   <=   writerIndex    <=    capacity
+ * 0      <=      reader_index   <=   writer_index    <=    capacity
  * @endcode
  */
 class Buffer {
@@ -25,8 +25,8 @@ class Buffer {
 
         explicit Buffer(size_t initialSize=kInitialSize):
             buffer_(0 + initialSize),
-            readerIndex_(0),
-            writerIndex_(0)
+            reader_index_(0),
+            writer_index_(0)
         {}
 
         /**
@@ -49,26 +49,41 @@ class Buffer {
          */
         void ensureWritableBytes(int len);
 
-        void set_readerIndex(int readerIndex);
+        void set_reader_index(int reader_index);
 
-        int get_readerIndex();
+        int reader_index();
 
-        void set_writerIndex(int writerIndex);
+        void set_writer_index(int writer_index);
 
-        int get_writerIndex();
+        int writer_index();
 
-        void read(int len);
+        /**
+         * @brief 从reader_index_独处指定数目的字节，同时reader_index_=reader_index_+byteNumber.
+         */
+        std::string read(int len);
+        std::string read();
         int64_t readInt64();
         int32_t readInt32();
         int16_t readInt16();
         int8_t readInt8();
 
+        /**
+         * @brief 写入指定数目的字节，同时writer_index_=writer_index_ + byteNumber. 如果
+         *  wirterIndex_ > capacity，buffer_会自动增长
+         */
         void write(const char* data, int len);
         void write(const void* data, int len);
         void writeInt64(int64_t x);
         void writeInt32(int32_t x);
         void writeInt16(int16_t x);
         void writeInt8(int8_t x);
+
+        /**
+         * @brief capacity of buffer.
+         */
+        int capacity() const {
+            return buffer_.capacity();
+        }
 
     private:
         /**
@@ -93,9 +108,9 @@ class Buffer {
         // 一块连续的内存
         std::vector<char> buffer_;
         // 控制读缓冲区首地址
-        int readerIndex_;
+        int reader_index_;
         // 控制写缓冲区首地址
-        int writerIndex_;
+        int writer_index_;
 };
 
 #endif // BUFFER_H_
