4d0eaf8da732b7609b1754ca7f799a9fef6c89d5
zixie1991 <zixie1991@163.com>
2015-02-05 00:50:15 +0800

    update eventloop.h(.cpp)

--- src/eventloop.cpp
+++ src/eventloop.cpp
@@ -1,1 +1,43 @@
 #include "eventloop.h"
+
+#include "poller.h"
+#include "channel.h"
+
+const int kPollTimeOutMs = 10000;
+
+EventLoop::EventLoop():
+    looping_(false),
+    quit_(false),
+    poller_(new Poller(this))
+{
+}
+
+EventLoop::~EventLoop() {
+    assert(!looping_);
+}
+
+void EventLoop::loop() {
+    looping_ = true;
+    quit_ = false;
+
+    while (!quit_) {
+        active_channels_.clear();
+        poller_->poll(kPollTimeOutMs, active_channels_);
+
+        for (std::vector<Channel*>::iterator it = active_channels_.begin();
+                it != active_channels_.end(); ++it) {
+            (*it)->handleEvent();
+        }
+
+    }
+
+    looping_ = false;
+}
+
+void EventLoop::quit() {
+    quit_ = true;
+}
+
+void EventLoop::updateChannel(Channel* channel) {
+    poller_->updateChannel(channel);
+}
--- src/eventloop.h
+++ src/eventloop.h
@@ -1,4 +1,33 @@
 #ifndef EVENTLOOP_H_
 #define EVENTLOOP_H_
 
+#include <vector>
+
+#include <boost/scoped_ptr.hpp>
+
+class Channel;
+class Poller;
+
+class EventLoop {
+    public:
+        EventLoop();
+        ~EventLoop();
+
+        /**
+         * @brief Loops foever.
+         */
+        void loop();
+
+        void quit();
+
+        // internal use only
+        void updateChannel(Channel* channel);
+
+    private:
+        bool looping_;
+        bool quit_;
+        boost::scoped_ptr<Poller> poller_;
+        std::vector<Channel*> active_channels_;
+};
+
 #endif // EVENTLOOP_H_
