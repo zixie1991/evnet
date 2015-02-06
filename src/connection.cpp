f6ed9726c6fdd6ebbb71aadad40478a4fcac3a46
zixie1991 <zixie1991@163.com>
2015-02-07 03:46:54 +0800

    update connection.cpp

--- src/connection.cpp
+++ src/connection.cpp
@@ -4,6 +4,7 @@
 
 #include "socket.h"
 #include "channel.h"
+#include "log.h"
 
 
 Connection::Connection(EventLoop* loop, std::string name, int sockfd, const InetAddress& peeraddr):
@@ -13,19 +14,25 @@ Connection::Connection(EventLoop* loop, std::string name, int sockfd, const Inet
     socket_(new Socket(sockfd)),
     channel_(new Channel(loop, sockfd))
 {
-    socket_->setNonblock();
-
+    log_debug("Connection constructor: name:%s, fd:%d", name_.c_str(), socket_->fd());
     channel_->set_read_callback(boost::bind(&Connection::handleReadEvent, this));
 }
 
 Connection::~Connection() {
-
+    log_debug("Connection destructor: name=%s, fd=%d", name_.c_str(), socket_->fd());
 }
 
 void Connection::connectionEstablished() {
     channel_->enableReadEvent();
+    //connection_callback_(this);
+}
 
-    connection_callback_(this);
+void Connection::set_connection_callback(const ConnectionCallback& cb) {
+    connection_callback_ = cb;
+}
+
+void Connection::set_message_callback(const MessageCallback& cb) {
+    message_callback_ = cb;
 }
 
 void Connection::handleReadEvent() {
@@ -34,5 +41,6 @@ void Connection::handleReadEvent() {
     n = ::read(socket_->fd(), buf, sizeof(buf) - 1);
     buf[n] = '\0';
 
+    log_debug("handle read event: received %d bytes: %s\n", n, buf);
     message_callback_(this, buf, n);
 }
