6a40e47282b09e060426ad9a0e1d6c8c6bc4c9fc
zixie1991 <zixie1991@163.com>
2015-02-17 15:40:19 +0800

    add testcase for runInLoop, queueInLoop

--- src/test/eventloop_test_.cpp
+++ src/test/eventloop_test_.cpp
@@ -4,9 +4,20 @@
 
 #include <iostream>
 
+#include <boost/bind.hpp>
+
 #include "eventloop.h"
+#include "log.h"
+
+using boost::bind;
+
+void print() {
+    log_info("hello world, in queue");
+}
 
 int main() {
     EventLoop loop;
+    loop.runInLoop(bind(&print));
+    loop.queueInLoop(bind(&print));
     loop.loop();
 }
