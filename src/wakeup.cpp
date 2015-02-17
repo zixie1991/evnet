12d9636f533d431422e30b1cb4babd94310726b9
zixie1991 <zixie1991@163.com>
2015-02-17 15:39:32 +0800

    add wakeup the loop

---
+++ src/wakeup.cpp
@@ -0,0 +1,40 @@
+#include "wakeup.h"
+
+#include <sys/eventfd.h>
+
+#include <boost/bind.hpp>
+
+#include "channel.h"
+#include "log.h"
+
+using boost::bind;
+
+Wakeup::Wakeup(EventLoop* loop):
+    loop_(loop),
+    wakeupfd_(::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC)),
+    wakeupfd_channel_(new Channel(loop, wakeupfd_))
+{
+    wakeupfd_channel_->set_read_callback(bind(&Wakeup::handleReadEvent, this));
+}
+
+Wakeup::~Wakeup() {
+    ::close(wakeupfd_);
+}
+
+void Wakeup::notify() {
+    uint64_t one = 1;    
+    int n = ::write(wakeupfd_, &one, sizeof(one));
+
+    if (sizeof(one) != n) {
+        log_error("wakeupfd_ write");
+    }
+}
+
+void Wakeup::handleReadEvent() {
+    uint64_t one = 1;
+    int n = ::read(wakeupfd_, &one, sizeof(one));
+
+    if (sizeof(one) != n) {
+        log_error("wakeupfd_ handle read event");
+    }
+}
---
+++ src/wakeup.h
@@ -0,0 +1,27 @@
+#ifndef WAKEUP_H_
+#define WAKEUP_H_
+
+#include <boost/scoped_ptr.hpp>
+
+class EventLoop;
+class Channel;
+
+class Wakeup {
+    public:
+        Wakeup(EventLoop* loop);
+        ~Wakeup();
+
+        // notify event
+        void notify();
+
+    private:
+        // called when eventfd notify
+        void handleReadEvent();
+
+        EventLoop* loop_;
+        // event notification file descriptor, create by eventfd().
+        int wakeupfd_;
+        boost::scoped_ptr<Channel> wakeupfd_channel_;
+};
+
+#endif // WAKEUP_H_
